4be56f84a954290fd7c58f6fe0721dd658821d7a
Sergio Ananias <sergio.ananias.o@gmail.com>
2021-05-07 12:05:42 -0400

    Se agrega tarjeta con datos

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-$('document').ready(function(){
-    const stark = 362;
-    const targaryen = 378;
-    const lannister = 229;
-    const baratheon = 17;
-    $('#stark').on('click', function(){
+$('document').ready(function () {
 
-        $.get(`https://anapioficeandfire.com/api/houses/${stark}`, function(data){
-            console.log(data)
+    $('.house').on('click', function () {
+        let id = $(this).attr("id")
+        let src = $(this).attr("src")
+        
+        $.get(`https://anapioficeandfire.com/api/houses/${id}`, function(data){
+            $('#house-details').html(`
+            <div class="card offset-5 p-3" style="width: 20rem;">
+                <img class="card-img-top" src="${src}"  alt="Card image cap"><div class="card-body">
+                <h5 class="card-title text-center">Nombre de la casa:</h5>
+                <h6 class="text-center">${data.name}</h6>
+                <ul class='card-text'>
+                    <li>Lema: ${data.words}</li>
+                    <li>Titulos:</li>
+                        <ul id="titles"></ul>
+                    </ul>
+            </div>`)
+            for(let i of data.titles){
+                $('#titles').append(`<li>${i}</li>`);
+            }
         }, 'JSON');
     });
 });
